f0d9fbf7ae99ec0095f0c5d655e2a233b62da0b2
Shaik Rameez <shaikrameez1803@gmail.com>
2025-12-29 21:58:15 +0530

    Day 8 | Functions, Parameters & Caesar Cipher
    Learned how to use functions with parameters, positional and keyword arguments.
    Built a Caesar Cipher program to encrypt and decrypt messages using function inputs.

--- DAY08/main.py
+++ DAY08/main.py
@@ -6,7 +6,7 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+# #TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
